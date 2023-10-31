ff545a92d508a8d0d3a290ab9ce72551da61c2dc
Reis567 <matheusdosreis9@gmail.com>
2023-10-31 18:40:55 -0300

    input text color

--- todo_project/src/components/ToDoForm.jsx
+++ todo_project/src/components/ToDoForm.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const TodoForm = ()=>{
+    const [value, setValue] = useState("")
     return (
         <form className='TodoForm'>
-            <input type="text"  className='todo-input' placeholder='Adicione uma tarefa ...'/>
+            <input type="text"  className='todo-input' placeholder='Adicione uma tarefa ...' onChange={(e)=> console.log(e.target.value)}/>
             <button type='submit' className='todo-btn'>Adicionar</button>
         </form>
     )
